474968d8ad0782aa3d6f6c7af92dd03b6cae301f
MuriithiMark <markmwangi14317@gmail.com>
2023-10-20 15:56:03 +0300

    Added SignIn, SignOut, UserProfile as exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,10 @@
 export { default as SignedIn } from './clerk/SignedIn.svelte';
 export { default as SignedOut } from './clerk/SignedOut.svelte';
-export { default as UserButton } from './clerk/UserButton.svelte'
+export { default as UserButton } from './clerk/UserButton.svelte';
+
+export { default as UserProfile } from './clerk/UserProfile.svelte';
+export { default as SignIn } from './clerk/SignIn.svelte';
+export { default as SignUp } from './clerk/SignUp.svelte';
 
 export { default as ClerkStore } from './clerk/stores/clerk-store'
 
